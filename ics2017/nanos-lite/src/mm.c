0170795b034b9c53381c84a55a222d4808fc3cbf
zwk2875206963 <zwk2875206963@gmail.com>
2018-11-03 21:00:35 +0800

    second commit

---
+++ ics2017/nanos-lite/src/mm.c
@@ -0,0 +1,62 @@
+#include "proc.h"
+#include "memory.h"
+
+static void *pf = NULL;
+
+void* new_page(void) {
+  assert(pf < (void *)_heap.end);
+  void *p = pf;
+  pf += PGSIZE;
+  return p;
+}
+
+void free_page(void *p) {
+  panic("not implement yet");
+}
+
+/* The brk() system call handler. */
+int mm_brk(uint32_t new_brk) {
+  if (current->cur_brk == 0) {
+
+    current->cur_brk = current->max_brk = new_brk;
+
+  }
+
+  else {
+
+    if (new_brk > current->max_brk) {
+
+      // TODO: map memory region [current->max_brk, new_brk)
+
+      // into address space current->as
+
+      if (new_brk & (PGSIZE - 1))
+
+        new_brk = (new_brk & ~(PGSIZE - 1)) + PGSIZE;
+
+      if (current->cur_brk & (PGSIZE - 1))
+
+        current->cur_brk = (current->cur_brk & ~(PGSIZE - 1)) + PGSIZE;
+
+      for (; current->cur_brk < new_brk; current->cur_brk += PGSIZE)
+
+        _map(&current->as, (void *)current->cur_brk, new_page());
+
+      current->max_brk = new_brk;
+
+    }
+
+
+
+    current->cur_brk = new_brk;
+
+  }
+  return 0;
+}
+
+void init_mm() {
+  pf = (void *)PGROUNDUP((uintptr_t)_heap.start);
+  Log("free physical pages starting from %p", pf);
+
+  _pte_init(new_page, free_page);
+}
---
+++ ics2017/nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -0,0 +1,54 @@
+#include <am.h>
+#include <x86.h>
+
+#define RTC_PORT 0x48   // Note that this is not standard
+#define I8042_DATA_PORT 0x60
+
+#define I8042_STATUS_PORT 0x64
+
+static unsigned long boot_time;
+
+void _ioe_init() {
+  boot_time = inl(RTC_PORT);
+}
+
+unsigned long _uptime() {
+  return inl(0x48) - boot_time;
+}
+
+uint32_t* const fb = (uint32_t *)0x40000;
+
+_Screen _screen = {
+  .width  = 400,
+  .height = 300,
+};
+
+extern void* memcpy(void *, const void *, int);
+
+void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
+   int cp_bytes = sizeof(uint32_t) * (w < (_screen.width - x) ? w : (_screen.width - x));
+
+  for (int j = 0; j < h && y + j < _screen.height; j ++) {
+
+    memcpy(&fb[(y + j) * _screen.width + x], pixels, cp_bytes);
+
+    pixels += w;
+
+  }
+}
+
+void _draw_sync() {
+}
+
+int _read_key() {
+uint8_t impresskey = inb(I8042_STATUS_PORT);
+
+  if (impresskey) {
+
+	  return inl(I8042_DATA_PORT);
+
+  } else {
+  return _KEY_NONE;
+ }
+
+}
